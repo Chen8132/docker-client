a240759a6d79f7ab5f2261e7a1d62f275c1e16e2
chen8132 <490414498@qq.com>
2017-01-17 10:54:59 +0800

    main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	ContainerClient cc("http://192.168.200.137");
+	ContainerClient cc("http://127.0.0.1");
 	cc.list_container(true, -1, "", "", -1);
 	cc.inspect_container("thirsty_pare");
 
